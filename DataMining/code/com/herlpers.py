243003f9551483741d22a611cdc3d0a9fcfac11f
Gaurav Paruthi <gparuthi@si-rollercoaster-mp.si.umich.edu>
2012-12-21 12:15:49 -0500

    select cities stats added

--- DataMining/code/com/herlpers.py
+++ DataMining/code/com/herlpers.py
@@ -31,6 +31,60 @@ def merge(locs):
     print 'finished at : '+str(datetime.now()) 
     print 'time taken= ' + str (datetime.now()-start_time)
     return all_locs
+
+
+def merge_aloc(locs, oneloc):
+    start_time = datetime.now()
+    print 'start time is : '+ str(start_time)
+    all_locs = {}
+    i =0
+    for d in locs:
+        print 'processing file results from day: ' + str(i)
+        for time in d:
+            print 'time:' + str(time)
+            if time not in all_locs:
+                all_locs[time] = {}
+            loc = oneloc
+            if loc in d[time]:
+                if d[time][loc]>50:
+                    newloc = loc
+                    if newloc in all_locs[time]:
+                        all_locs[time][newloc] += d[time][loc]
+                    else:
+                        all_locs[time][newloc] = d[time][loc]
+            else:
+                all_locs[time][loc] = 0
+        i+=1
+    print 'finished at : '+str(datetime.now())
+    print 'time taken= ' + str (datetime.now()-start_time)
+    return all_locs
+
+def merge_locs(locs, loc_list):
+    start_time = datetime.now()
+    print 'start time is : '+ str(start_time)
+    all_locs = {}
+    i =0
+    for d in locs:
+        print 'processing file results from day: ' + str(i)
+        for time in d:
+            print 'time:' + str(time)
+            if time not in all_locs:
+                all_locs[time] = {}
+            for loc in loc_list:
+#            for loc in d[time]:
+#                if d[time][loc]>50:
+                newloc = loc
+                if loc in d[time]:
+                    if newloc in all_locs[time]:
+                        all_locs[time][newloc] += d[time][loc]
+                    else:
+                        all_locs[time][newloc] = d[time][loc]
+                else:
+                    print 'key error loc:%s, time:%s' %(loc,time)
+        i+=1
+    print 'finished at : '+str(datetime.now()) 
+    print 'time taken= ' + str (datetime.now()-start_time)
+    return all_locs
                 
 def temp():
     for k in all_locs:
