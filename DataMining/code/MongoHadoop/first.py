19d0cb1223a990ef8904f4180259d33af0702ef4
gparuthi <gparuthi@umich.edu>
2013-02-09 12:50:23 -0500

    added mongo db updates from parallel dm of tweets

--- DataMining/code/MongoHadoop/first.py
+++ DataMining/code/MongoHadoop/first.py
@@ -10,9 +10,48 @@ lview = rc.load_balanced_view()
 lview.block = True
 
 from DataMining.code.com.BigData import BigData
-# input_files = BigData.GetInputFiles('./DataMining/data/')
-input_files = ['./DataMining/data/gardenhose.2012-11-01.gz']
+input_files = BigData.GetInputFiles('./DataMining/data/')
+# input_files = ['./DataMining/data/gardenhose.2012-11-01.gz']
 
+
+keywords = ['barack', 'obama', 'mitt', 'romney', 'president', 'election']
+sel_cities = ['washington dc','boston ','atlanta ','boston','chicago ','new york','chicago','los angeles','toronto','london','rio','paris','mexico city','lima','singapore','tokyo','buenos aires','jakarta','bandung']
+def bdCheckCondition_keywords(rec,sel_cities):
+    if 'user' in rec:
+        if 'location' in rec['user']:
+            if (rec['user']['location'] != None):
+                if rec['user']['location'] in sel_cities:
+                    return True
+            else:
+                return False
+        return False
+
+def bdDoSomething_keywords(rec,locs,kwords):
+    # get time till hour                                                                                                                                                                                                                 
+    time = parse(rec['created_at']).replace(minute=0,second=0,tzinfo=None)
+    # location of rec                                                                                                                                                                                                                     
+    loc = rec['user']['location'].lower().replace('.','').replace(',','')
+
+    isin_kwords = False
+
+    for k in kwords:
+        if k in rec['text'].lower():
+            isin_kwords = True
+        else:
+            isin_kwords = False
+
+    if time in locs:
+        #initialize the location for the given time if not found
+        if loc not in locs[time]:
+            locs[time][loc] = {'kwords':0,'others':0}
+
+        if isin_kwords:
+            locs[time][loc]['kwords'] += 1
+        else:
+            locs[time][loc]['others'] += 1
+
+    else:
+        locs[time] = {}
 
 def bdCheckCondition(rec):
     if 'user' in rec:
@@ -23,12 +62,22 @@ def bdCheckCondition(rec):
                 return False
         return False
 
-def doSomething(rec):
+def doSomething(rec, db, filep):
+	# get tweet id, the curent file path
+	t_id = rec['id']
 	# store the rec to mongodb
-	db.test.insert(rec)
+	db.tweets.insert({'tweet_id':t_id, 'filep':filep})
+	time = parse(rec['created_at']).replace(minute=0,second=0,tzinfo=None)
+    # location of rec                                                                                                                                                                                                                     
+    loc = rec['user']['location'].lower().replace('.','').replace(',','')
+
+    # if the time and loc already existing then increase count else insert new count
+    db.timeline.update({'time':time, 'loc':loc}, {'$inc':{'count':1}}, True)
+
+
 
 @lview.parallel()
-def processFile(filep):
+def processFile(filep, bdCheckCondition, doSomething):
         from DataMining.code.com import log, parallels
         import os
         from ujson import loads
@@ -50,9 +99,9 @@ def processFile(filep):
             #print line                                                                                               
             rec = loads(line)
             tot_lines += 1
-            condition = parallels.bdCheckCondition(rec)
+            condition = bdCheckCondition(rec)
             if condition:
-                db.test.insert(rec)
+                doSomething(rec, db, filep)
                 loc_lines += 1
                 if (loc_lines%10000==0):
                     logger.log('Count:' + str(loc_lines) + '/' + str(tot_lines))
@@ -62,5 +111,5 @@ def processFile(filep):
 
 print 'starting now..'
 starttime = datetime.now()
-res = processFile.map(input_files)
+res = processFile.map(input_files, bdCheckCondition, doSomething)
 print 'time taken= '+ str(datetime.now()-starttime)
