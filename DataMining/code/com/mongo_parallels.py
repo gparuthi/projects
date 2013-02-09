655cd493585ab3b27d33da5cb3cdbde180867974
gparuthi <gparuthi@umich.edu>
2013-02-09 13:04:30 -0500

    added mongo_parallels

---
+++ DataMining/code/com/mongo_parallels.py
@@ -0,0 +1,64 @@
+def bdCheckCondition(rec):
+    if 'user' in rec:
+        if 'location' in rec['user']:
+            if (rec['user']['location'] != None):
+                return True
+            else:
+                return False
+        return False
+
+def doSomething(rec, db, filep):
+	# get tweet id, the curent file path
+	t_id = rec['id']
+	# store the rec to mongodb
+	db.tweets.insert({'tweet_id':t_id, 'filep':filep})
+
+	time = parse(rec['created_at']).replace(minute=0,second=0,tzinfo=None)
+
+	# location of rec                                                                                                                                                                                                                     
+	loc = rec['user']['location'].lower().replace('.','').replace(',','')
+
+	# if the time and loc already existing then increase count else insert new count
+	db.timeline.update({'time':time, 'loc':loc}, {'$inc':{'count':1}}, True)
+
+
+	
+
+# keywords = ['barack', 'obama', 'mitt', 'romney', 'president', 'election']
+# sel_cities = ['washington dc','boston ','atlanta ','boston','chicago ','new york','chicago','los angeles','toronto','london','rio','paris','mexico city','lima','singapore','tokyo','buenos aires','jakarta','bandung']
+# def bdCheckCondition_keywords(rec,sel_cities):
+#     if 'user' in rec:
+#         if 'location' in rec['user']:
+#             if (rec['user']['location'] != None):
+#                 if rec['user']['location'] in sel_cities:
+#                     return True
+#             else:
+#                 return False
+#         return False
+
+# def bdDoSomething_keywords(rec,locs,kwords):
+#     # get time till hour                                                                                                                                                                                                                 
+#     time = parse(rec['created_at']).replace(minute=0,second=0,tzinfo=None)
+#     # location of rec                                                                                                                                                                                                                     
+#     loc = rec['user']['location'].lower().replace('.','').replace(',','')
+
+#     isin_kwords = False
+
+#     for k in kwords:
+#         if k in rec['text'].lower():
+#             isin_kwords = True
+#         else:
+#             isin_kwords = False
+
+#     if time in locs:
+#         #initialize the location for the given time if not found
+#         if loc not in locs[time]:
+#             locs[time][loc] = {'kwords':0,'others':0}
+
+#         if isin_kwords:
+#             locs[time][loc]['kwords'] += 1
+#         else:
+#             locs[time][loc]['others'] += 1
+
+#     else:
+#         locs[time] = {}
