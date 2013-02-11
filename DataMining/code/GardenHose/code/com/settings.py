9d9176b8acdad558756ca2d153aef6f611a852f7
Gaurav Paruthi <gparuthi@si-rollercoaster-mp.si.umich.edu>
2013-02-11 15:50:31 -0500

    - cleaned dir, - fixed All_locs

--- DataMining/code/GardenHose/code/com/settings.py
+++
@@ -1,3 +0,0 @@
-LOG_PATH_DIR = '/Users/gparuthi/Dropbox/dm_code/GardenHose/logs/'
-INPUT_DIR = '/Users/gparuthi/Dropbox/dm_code/GardenHose/data/'
-OUTPUT_DIR = '/Users/gparuthi/Dropbox/dm_code/GardenHose/output/'
